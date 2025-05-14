70a2324035e633fed6d3b3c7e7a11b82f742eef0
Lugnasin, Johnlee <johnlee.lugnasin@student.pnm.edu.ph>
2025-05-14 12:59:41 +0800

    Add files via upload

---
+++ Voting System Final/Admin.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Voting_System_Final
+{
+    partial class AdminForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.Showpass = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(116, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(174, 184);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(164, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(169, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Password";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Linen;
+            this.button2.Location = new System.Drawing.Point(93, 432);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(243, 41);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "LOGIN";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Linen;
+            this.button1.Location = new System.Drawing.Point(93, 494);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 41);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Username
+            // 
+            this.Username.Location = new System.Drawing.Point(89, 269);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(246, 20);
+            this.Username.TabIndex = 17;
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(89, 375);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(246, 20);
+            this.Password.TabIndex = 18;
+            this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            // 
+            // Showpass
+            // 
+            this.Showpass.AutoSize = true;
+            this.Showpass.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Showpass.Location = new System.Drawing.Point(271, 406);
+            this.Showpass.Name = "Showpass";
+            this.Showpass.Size = new System.Drawing.Size(56, 17);
+            this.Showpass.TabIndex = 19;
+            this.Showpass.Text = "Show ";
+            this.Showpass.UseVisualStyleBackColor = true;
+            this.Showpass.CheckedChanged += new System.EventHandler(this.Showpass_CheckedChanged);
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(427, 618);
+            this.Controls.Add(this.Showpass);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AdminForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Admin_form";
+            this.Load += new System.EventHandler(this.AdminForm_Load);
+            this.Click += new System.EventHandler(this.AdminForm_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.CheckBox Showpass;
+    }
+}
---
+++ Voting System Final/Admin.cs
@@ -0,0 +1,121 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Voting_System_Final
+{
+    public partial class AdminForm: Form
+    {
+        private string connectionString = "server=localhost;database=votingdb;username=root;password=;";
+        public AdminForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AdminForm_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AdminForm_Load(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string username = Username.Text;
+            string password = Password.Text;
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both username and password.");
+                return;
+            }
+
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // Assuming there is a separate `admin` table with plain text passwords
+                    string query = "SELECT FullName FROM admin WHERE Username = @username AND Password = @password";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@username", username);
+                        cmd.Parameters.AddWithValue("@password", password); // No hashing
+
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string fullName = reader["FullName"].ToString();
+                                MessageBox.Show("Admin login successful!\nWelcome, " + fullName);
+
+                                AdminDashboard adminDashboard = new AdminDashboard();
+                                adminDashboard.Show();
+
+                                this.Hide();
+                                // Open Admin Dashboard here if needed
+                            }
+                            else
+                            {
+                                MessageBox.Show("Invalid admin username or password.");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Database error: " + ex.Message);
+                }
+            }
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+            Password.UseSystemPasswordChar = true; // Hide password on form load
+        }
+
+
+        private void Showpass_CheckedChanged(object sender, EventArgs e)
+        {
+            // Show password only if checkbox is checked
+            if (Showpass.Checked)
+            {
+                Password.UseSystemPasswordChar = false; // Show password
+            }
+            else
+            {
+                Password.UseSystemPasswordChar = true; // Hide password
+            }
+        }
+
+        private void Password_Click(object sender, EventArgs e)
+        {
+            Password.UseSystemPasswordChar = true; // Force hide again on click
+            Showpass.Checked = false;              // Optional: uncheck the box
+        }
+
+    }
+}
